0cec7fb1c488983348d3528f4075c26d38c02524
yusuf.kocaman <yusuf.kocaman@msn.com>
2016-11-16 22:12:53 +0300

    proje için listeleme sayfası oluşturuldu

---
+++ Web/Controllers/ProjectsController.cs
@@ -0,0 +1,33 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class ProjectsController : BaseController
+    {
+        // GET: Projects
+        public ActionResult Index()
+        {
+            var projects = db.ProjectSet.AsQueryable();
+
+            return View(projects);
+        }
+
+        public ActionResult Download(int id)
+        {
+            var project = db.ProjectSet.FirstOrDefault(q => q.Id == id);
+            if (project == null)
+            {
+                return Content("Dosya bulunamadı");
+            }
+
+            Response.AddHeader("Content-Disposition", "attachment; filename=" + project.Title);
+
+            return File(project.File, project.ContentType);
+        }
+    }
+}
